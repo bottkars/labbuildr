6a58082ba9a0e6039ddfcfdf0a3bfedd3a9de76c
bottkars <Karsten.Bott@bottnet.de>
2014-10-07 22:49:38 +0200

    Sofs Setup

---
+++ Scripts/SOFS/new-sofsserver.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2014/06/16/announcement-labbuildr-released
+#>
+#requires -version 3
+[CmdletBinding()]
+param (
+[string]$SOFSNAME
+)
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+New-Item -ItemType file  "$Builddir\$ScriptName$Logtime.log"
+###########
+Add-ClusterScaleOutFileServerRole -Name $SOFSNAME
+
