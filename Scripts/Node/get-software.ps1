98d536984954623912fad94f5a43ae66685ab398
bottkars <Karsten.Bott@bottnet.de>
2014-11-05 17:57:59 +0100

    diskspd and software inventory

---
+++ Scripts/Node/get-software.ps1
@@ -0,0 +1,19 @@
+﻿param(
+$computername = $null
+)
+
+$code = {
+get-itemproperty 'hklm:\software\microsoft\windows\currentversion\uninstall\*' |where-object { $_.DisplayName } |Select-Object DisplayName, DisplayVersion, Publisher
+}
+
+if ($computername)
+{
+Invoke-Command -ScriptBlock $code -computerName $computername |
+Select-Object DisplayName, DisplayVersion, Publisher
+} 
+else 
+{
+    Invoke-Command -ScriptBlock $code
+}
+
+
