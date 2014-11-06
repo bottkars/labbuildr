3a5345eacabd271269c925f2aaea77d8dce7780f
bottkars <Karsten.Bott@bottnet.de>
2014-11-06 13:19:09 +0100

    scaleio linux nodes

---
+++ Scripts/Node/set-defaultgateway.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.Synopsis
+   Change Default gateway for Known Hosts
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   set-defaultgateway.ps1 -Computername HVNode3 -gateway 10.10.0.3
+.EXAMPLE
+#>
+
+
+param (
+    $gateway = "10.10.0.103",
+    $Computername = $null
+    )
+
+
+
+$ScriptBlock = {
+param 
+    (
+    $Gateway
+    )
+$Newgateway = (Get-WmiObject -Class win32_networkAdapterconfiguration | where InterfaceIndex -Match (Get-NetIPAddress -AddressState Preferred -InterfaceAlias "Ethernet*" -SkipAsSource $false -AddressFamily IPv4 | where Prefixorigin -ne "DHCP").InterfaceIndex).SetGateways("$Gateway") 
+(Get-WmiObject -Class win32_networkAdapterconfiguration | where InterfaceIndex -Match (Get-NetIPAddress -AddressState Preferred -InterfaceAlias "Ethernet*" -SkipAsSource $false -AddressFamily IPv4 | where Prefixorigin -ne "DHCP").InterfaceIndex) | Select-Object -SkipLast 1 PSComputername, DefaultIPGateway 
+} # end Scriptblock
+
+if ($Computername)
+    {
+    Invoke-Command -ScriptBlock $ScriptBlock -computerName $Computername -ArgumentList $gateway
+    } 
+else 
+    {
+    Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $gateway
+    }
