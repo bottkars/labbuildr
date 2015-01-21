b628fc724f5bc35f010bfb195232d2f69d0aa6e4
bottkars <Karsten.Bott@bottnet.de>
2015-01-21 09:10:19 +0100

    added configure-nfs to set nfs on dcnode

---
+++ Scripts/DC/configure-nfs.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2014/06/16/announcement-labbuildr-released
+#>
+#requires -version 3
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Dirname = "nfs"
+$Sharename = $env:USERDOMAIN+$dirname
+$nfsdir = join-path $env:SystemDrive $Dirname
+Add-WindowsFeature fs-nfs-service -IncludeManagementTools
+New-Item -ItemType directory c:\nfs
+New-NfsShare -Name $sharename -Path $nfsdir  -Permission readwrite  -Authentication sys -unmapped $true -AllowRootAccess $true
