dd30ee1171a6887f0d5104a7367fbd124a6ffd5a
bottkars <Karsten.Bott@bottnet.de>
2015-01-22 10:31:45 +0100

    added recurse copy for source attachements configure note changed to eval neics on name sorted

--- Scripts/Panorama/panorama.ps1
+++ Scripts/Panorama/panorama.ps1
@@ -11,14 +11,20 @@
 param(
 
 )
+$Attachements = "\\vmware-host\shared folders\Sources\Attachements"
 $ScriptName = $MyInvocation.MyCommand.Name
 $Host.UI.RawUI.WindowTitle = "$ScriptName"
 $Builddir = $PSScriptRoot
 New-Item -ItemType file  "$Builddir\$ScriptName.log"
 $SMBSERVER = Split-Path -Leaf $env:LOGONSERVER
 Invoke-Command -ComputerName $SMBSERVER -ScriptBlock {
-    New-Item -ItemType Directory -Path c:\panorama
-    New-SmbShare -FullAccess "Domain Users" -Path c:\panorama -Name Panorama
+    New-Item -ItemType Directory -Path c:\panorama -ErrorAction SilentlyContinue
+    New-SmbShare -FullAccess "Domain Users" -Path c:\panorama -Name Panorama -ErrorAction SilentlyContinue
+    }
+### do we have attachements available ?
+if (Test-Path $Attachements)
+    {
+    Get-ChildItem -Path $Attachements | % { Copy-Item $_.fullname (Join-Path $env:LOGONSERVER Panorama) -Recurse -Force -PassThru }
     }
 Start-Process -FilePath "msiexec.exe" -ArgumentList '/i "\\vmware-host\Shared Folders\Sources\Panorama\Syncplicity Panorama.msi" /passive' -PassThru -Wait
 Start-Process http://localhost:9000
