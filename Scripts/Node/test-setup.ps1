71034f7e1c0c225c115c2f2ff812aa1aa30bda5b
bottkars <Karsten.Bott@bottnet.de>
2015-01-23 17:13:34 +0100

    new setup precheck routines to test sources

---
+++ Scripts/Node/test-setup.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2014/06/16/announcement-labbuildr-released
+#>
+#requires -version 3
+[CmdletBinding()]
+param (
+[parameter(Mandatory = $true)]$setup,
+[parameter(Mandatory = $true)]$setuppath
+
+)
+do {
+
+    $pathok = Test-Path $Setuppath
+    if ($pathok -notmatch $True)
+        { 
+        write-warning "we can not find $setup Sources $Setuppath ! Make sure the you have downloaded the required sources"
+        write-warning "Script will continue once the required $setup Sources are in available "
+        $([char]7)
+        Start-Sleep -Seconds 5
+        }
+    }
+until ($pathok -match $true)
+write-warning "found $setuppath"
