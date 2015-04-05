41c3c68a19878a7ef9c455b2e2aa57552e66f434
bottkars <Karsten.Bott@bottnet.de>
2015-04-05 17:29:30 +0200

    modified Exchange Attachement Processing clening of old content in build-lab

--- Scripts/Exchange/user.ps1
+++ Scripts/Exchange/user.ps1
@@ -29,31 +29,26 @@ $Subject = "Welcome to $Domain"
 $SenderSMTP = "Administrator"+$maildom
 $Smtpserver = $env:COMPUTERNAME+$Dot+$ADDomain
 $BackupAdmin = "NMMBackupUser"
-$Body = "Hello and have fun ... for Questions drop an email to Karsten.Bott@emc.com "
-$AttachDir =  Join-Path $Builddir Attachements 
+$Body = "Welcome to Exchange at $Domain
+Enjoy the new Features
+Trry Neworker and/or Avamar with the new Environment !
+... for Questions drop an email to Karsten.Bott@emc.com
+Follow me on twritter @Hyperv_Guy
+Make sure to Rate in my Blog !
+https://community.emc.com/blogs/bottk/2014/06/16/announcement-labbuildr-released
+"
+$AttachDir =  '\\vmware-host\Shared Folders\Sources\Attachements'
 $PlainPassword = "Password123!"
 $DomainUser = "$Domain\Administrator"
 $SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $DomainUser, $SecurePassword
-function Extract-Zip
-{
-    param([string]$zipfilename, [string] $destination)
-    $copyFlag = 16 # overwrite = yes 
-    $Origin = $MyInvocation.MyCommand
-	if(test-path($zipfilename))
-	{	
-		$shellApplication = new-object -com shell.application
-		$zipPackage = $shellApplication.NameSpace($zipfilename)
-		$destinationFolder = $shellApplication.NameSpace($destination)
-		$destinationFolder.CopyHere($zipPackage.Items(),$copyFlag)
-	}
-}
 Set-TransportConfig -MaxReceiveSize 50MB
 Set-TransportConfig -MaxSendSize 50MB
 Enable-Mailbox -Identity $BackupAdmin
-New-Item -ItemType Directory $AttachDir
-Extract-Zip c:\scripts\attachements.zip $AttachDir
-$Attachement = Get-ChildItem -Path $AttachDir -file -Filter *microsoft*release-notes*
+if (Test-Path $AttachDir)
+    {
+    $Attachement = Get-ChildItem -Path $AttachDir -Recurse -file -Filter *microsoft*release-notes*
+    }
 $RoleGroup = "EMC NMM Exchange Admin Roles"
 $Roles = ("Database Copies", "Databases", "Disaster Recovery", "Mailbox Import Export", "Mail Recipient Creation", "Mail Recipients", "View-Only Configuration", "View-Only Recipients")
 New-RoleGroup -Name $RoleGroup -DisplayName $RoleGroup -Members $BackupAdmin -Roles $Roles -Description "This role group allows its users to perform database recovery and GLR"
@@ -66,38 +61,47 @@ Restart-Service MSExchangeIS
 Get-AddressList  | Update-AddressList
 # Enable-Mailbox -Identity $Domain\Administrator
 # New-ManagementRoleAssignment -Role "Databases" -User $BackupAdmin
-Send-MailMessage -From $SenderSMTP -Subject $Subject -To "$BackupAdmin$maildom"  -Body $Body -Attachments $Attachement.FullName -DeliveryNotificationOption None -SmtpServer $Smtpserver -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+if ($Attachement)
+    {
+    Send-MailMessage -From $SenderSMTP -Subject $Subject -To "$BackupAdmin$maildom"  -Body $Body -Attachments $Attachement[0].FullName -DeliveryNotificationOption None -SmtpServer $Smtpserver -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    }
+else
+    {
+    Send-MailMessage -From $SenderSMTP -Subject $Subject -To "$BackupAdmin$maildom"  -Body $Body -DeliveryNotificationOption None -SmtpServer $Smtpserver -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    }
 get-ExchangeServer  | add-adpermission -user $BackupAdmin -accessrights ExtendedRight -extendedrights Send-As, Receive-As, ms-Exch-Store-Admin
-
-
-if (Get-DatabaseAvailabilityGroup){
-$DAGDatabase = Get-MailboxDatabase | where ReplicationType -eq Remote
-$Database = $DAGDatabase.Name}
-Import-CSV C:\Scripts\user.csv | ForEach {
-$givenname=$_.givenname
-$surname=$_.surname
-$Displayname = $givenname+$Space+$surname
-$SamAccountName = $Givenname.Substring(0,1)+$surname
-$UPN = $SamAccountName+$maildom
-$emailaddress = "$givenname$Dot$surname$maildom"
-$name = "$givenname $surname"
-$user = @{
-givenname=$givenname;
-surname=$surname;
-name=$name;
-displayname=$Displayname;
-samaccountname=$SamAccountName;
-userprincipalname=$UPN;
-emailaddress=$emailaddress;
-homedirectory=" ";
-accountpassword=(ConvertTo-SecureString "Welcome1" -AsPlainText -Force);
-}
-New-ADUser @user -Enabled $True
-Enable-Mailbox $user.samaccountname -database $Database
-Send-MailMessage -From $SenderSMTP -Subject $Subject -Attachments $Attachement.FullName -To $UPN -Body $Body -DeliveryNotificationOption None -SmtpServer $Smtpserver -Credential $Credential -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-}
-
-
+if (Get-DatabaseAvailabilityGroup)
+    {
+    $DAGDatabase = Get-MailboxDatabase | where ReplicationType -eq Remote
+    $Database = $DAGDatabase.Name}
+    $Users = Import-CSV C:\Scripts\user.csv 
+    if (Test-Path '\\vmware-host\Shared Folders\Sources\customusers.csv')
+        {
+        $Users += Import-CSV '\\vmware-host\Shared Folders\Sources\customusers.csv'
+        }
+    $Users | ForEach {
+        $givenname=$_.givenname
+        $surname=$_.surname
+        $Displayname = $givenname+$Space+$surname
+        $SamAccountName = $Givenname.Substring(0,1)+$surname
+        $UPN = $SamAccountName+$maildom
+        $emailaddress = "$givenname$Dot$surname$maildom"
+        $name = "$givenname $surname"
+        $user = @{
+            givenname=$givenname;
+            surname=$surname;
+            name=$name;
+            displayname=$Displayname;
+            samaccountname=$SamAccountName;
+            userprincipalname=$UPN;
+            emailaddress=$emailaddress;
+            homedirectory=" ";
+            accountpassword=(ConvertTo-SecureString "Welcome1" -AsPlainText -Force);
+            }
+        New-ADUser @user -Enabled $True
+        Enable-Mailbox $user.samaccountname -database $Database
+        Send-MailMessage -From $SenderSMTP -Subject $Subject -Attachments $Attachement.FullName -To $UPN -Body $Body -DeliveryNotificationOption None -SmtpServer $Smtpserver -Credential $Credential -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    }
 #######
 ##Public Folder Structure
 New-Mailbox -PublicFolder -Name $Domain -database $Database 
@@ -106,53 +110,45 @@ Enable-MailPublicFolder $Newfolder
 # fixing the DSN 5.7.1. Create Item Change since Cu4
 Add-PublicFolderClientPermission $Newfolder -User ANONYMOUS -AccessRights createitems
 $PFSMTP = (Get-MailPublicFolder -Identity $Newfolder).EMAILAddresses[0].AddressString
-$count = (Get-ChildItem -Path $AttachDir -file).count
+$Attachements = Get-ChildItem -Path $AttachDir -Recurse -file
+$count = $Attachements.count
 $incr = 1
-foreach ( $file in Get-ChildItem -Path $AttachDir -file ) {
-Write-Progress -Activity "Sending File to Public Folder $Newfolder " -Status $file -PercentComplete (100/$count*$incr)
-Send-MailMessage -From $SenderSMTP -Subject $file.name -To $PFSMTP -Attachments $file.FullName -DeliveryNotificationOption None -SmtpServer $Smtpserver -Credential $Credential -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-$incr++
-}
+foreach ($file in $Attachements) {
+    Write-Progress -Activity "Sending $File to Public Folder $Newfolder " -Status $file -PercentComplete (100/$count*$incr)
+    Send-MailMessage -From $SenderSMTP -Subject $file.name -To $PFSMTP -Attachments $file.FullName -DeliveryNotificationOption None -SmtpServer $Smtpserver -Credential $Credential -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    $incr++
+    }
 Import-CSV C:\Scripts\folders.csv | ForEach {
-$Folder=$_.Folder
-$Path=$_.Path -replace "BRSLAB", "$Domain" 
-$Path 
-New-PublicFolder -Name $Folder -Path $Path
-Enable-MailPublicFolder "$Path\$Folder"
-# Add-PublicFolderClientPermission $Path\$Folder -User Anonymous -AccessRights createitems
-
-Send-MailMessage -From $SenderSMTP -Subject "Welcome To Public Folders" -To $Folder$maildom -Body "This is Public Folder $Folder" -DeliveryNotificationOption None -SmtpServer $Smtpserver -Credential $Credential -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    $Folder=$_.Folder
+    $Path=$_.Path -replace "BRSLAB", "$Domain" 
+    $Path 
+    New-PublicFolder -Name $Folder -Path $Path
+    Enable-MailPublicFolder "$Path\$Folder"
+    Send-MailMessage -From $SenderSMTP -Subject "Welcome To Public Folders" -To $Folder$maildom -Body "This is Public Folder $Folder" -DeliveryNotificationOption None -SmtpServer $Smtpserver -Credential $Credential -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
 }
 #Get-ReceiveConnector “*Default Frontend*" | Add-ADPermission -User “NT AUTHORITY\ANONYMOUS LOGON” -ExtendedRights “Ms-Exch-SMTP-Accept-Any-Recipient”
 
 ################# Configuring SMTP receive Connector for non Exchange Servers and setting Up Mailhost on DNS
 #Write-Host -ForegroundColor Yellow "Setting Up receive Connector"
-
 # New-ReceiveConnector -Name SMTP -Usage Custom -Bindings $MyIP":25" -RemoteIPRanges "$Subnet.1-$Subnet.99","$Subnet.110-$Subnet.255"
 # Get-ReceiveConnector “*Default Frontend*" | Add-ADPermission -User “NT AUTHORITY\ANONYMOUS LOGON” -ExtendedRights “Ms-Exch-SMTP-Accept-Any-Recipient”
 # Get-ReceiveConnector | where identity -match "Default Frontend" | Add-ADPermission -User "NT AUTHORITY\ANONYMOUS LOGON" -ExtendedRights "Ms-Exch-SMTP-Accept-Any-Recipient"
-
-
-
 Write-Host -ForegroundColor Yellow "Setting Up C-record for mailhost"
 If ($AddressFamily -match 'IPv4')
-{
-$dnsserver = (Get-DnsClientServerAddress -AddressFamily  IPv4 | where ServerAddresses -match $Subnet).ServerAddresses[0]
-$zone = get-dnsserverzone (Get-ADDomain).dnsroot -ComputerName $dnsserver
-Add-DnsServerResourceRecordCName -HostNameAlias "$env:COMPUTERNAME.$ADDomain" -Name mailhost -ZoneName $zone.ZoneName -ComputerName $dnsserver
+    {
+    $dnsserver = (Get-DnsClientServerAddress -AddressFamily  IPv4 | where ServerAddresses -match $Subnet).ServerAddresses[0]
+    $zone = get-dnsserverzone (Get-ADDomain).dnsroot -ComputerName $dnsserver
+    Add-DnsServerResourceRecordCName -HostNameAlias "$env:COMPUTERNAME.$ADDomain" -Name mailhost -ZoneName $zone.ZoneName -ComputerName $dnsserver
 
-}
+    }
 If ($AddressFamily -match 'IPv6')
-{ 
-
-$dnsserver = (Get-DnsClientServerAddress -AddressFamily  IPv6 | where ServerAddresses -match $IPv6Prefix).ServerAddresses[0]
-$zone = get-dnsserverzone (Get-ADDomain).dnsroot -ComputerName $dnsserver
-Add-DnsServerResourceRecordCName -HostNameAlias "$env:COMPUTERNAME.$ADDomain" -Name mailhost -ZoneName $zone.ZoneName -ComputerName $dnsserver
-
-}
+    { 
+    $dnsserver = (Get-DnsClientServerAddress -AddressFamily  IPv6 | where ServerAddresses -match $IPv6Prefix).ServerAddresses[0]
+    $zone = get-dnsserverzone (Get-ADDomain).dnsroot -ComputerName $dnsserver
+    Add-DnsServerResourceRecordCName -HostNameAlias "$env:COMPUTERNAME.$ADDomain" -Name mailhost -ZoneName $zone.ZoneName -ComputerName $dnsserver
+    }
 
 if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
     {
     Pause
     }
-# Remove-PSSession $Session
