c494b5e0d044c6effac5f44dadb78ccfe991f1b7
bottkars <Karsten.Bott@bottnet.de>
2015-05-15 12:25:37 +0200

    changed outbut of backupwatcher build-lab now on nw branch 2

--- Scripts/Node/watch-backupevents.ps1
+++ Scripts/Node/watch-backupevents.ps1
@@ -14,5 +14,5 @@ param (
 #requires -version 3
 
 $Host.UI.RawUI.WindowTitle = "$Computername"
-do {Get-EventLog -LogName Application -Newest 20 -ComputerName $Computername -Source VSS,Networker,Nmm | ft TimeGenerated,EventID, Source, Message -AutoSize ; sleep 5 ;Clear-Host } 
+do {Get-EventLog -LogName Application -Newest 20 -ComputerName $Computername -Source VSS,Networker,Nmm | Sort-Object Time -Descending | ft Time, EntryType, Source, Message -AutoSize; sleep 5 ;Clear-Host } 
 until ($false)
