30ac9257876b5a76655d72312781c61f48b63d7f
bottkars <Karsten.Bott@bottnet.de>
2015-05-21 21:53:50 +0200

    step checker new config for standard cloud in vmm

---
+++ Scripts/Node/set-step.ps1
@@ -0,0 +1,20 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2014/06/16/announcement-labbuildr-released
+#>
+param (
+$step,
+[switch]$reboot)
+
+#requires -version 3
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+New-Item -ItemType file  "$Builddir\$ScriptName$Logtime.log"
+New-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name "Pass$step" -Value "$PSHOME\powershell.exe -Command `"New-Item -ItemType File -Path c:\scripts\$step.pass`""
+if ($reboot.IsPresent){restart-computer -force}
