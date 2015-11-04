49bdca7c2fda9335c020526679e7efc8f81b588a
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-04 11:14:23 +0100

    First Production version of SCOM Installer

--- Scripts/SCOM/install-scom.ps1
+++ Scripts/SCOM/install-scom.ps1
@@ -20,6 +20,16 @@ $Builddir = $PSScriptRoot
 $Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
 New-Item -ItemType file  "$Builddir\$ScriptName$Logtime.log"
 
+$Domain = $($Env:USERDOMAIN)
+$INSTANCENAME="$($Env:COMPUTERNAME)\MSSQL$Domain"
+$Action_ACT = "$($Domain)\Administrator"
+$DAS_ACT = "$($Domain)\SVC_SQLADM"
+$Data_Reader = "$($Domain)\SVC_SQLADM"
+$Data_Writer = "$($Domain)\SVC_SQLADM"
+$Password = "Password123!"
+$MGMTGrp = "$($Domain)Mgmt"
+$Components = "OMServer,OMConsole"
+
 .$Builddir\test-sharedfolders.ps1
 
 $Setupcmd = "SQLSysClrTypes.msi"
@@ -28,13 +38,32 @@ $Setuppath = "$SourcePath\$SCOMver$Prereq\$Setupcmd"
 Write-Warning "Starting SQL Cleartype Setup"
 Start-Process $Setuppath -ArgumentList "/q"
 
-
 $Setupcmd = "ReportViewer.msi"
 $Setuppath = "$SourcePath\$SCOMver$Prereq\$Setupcmd"
 .$Builddir\test-setup -setup $Setupcmd -setuppath $Setuppath
 Write-Warning "Starting Report Viewer Setup"
 Start-Process $Setuppath -ArgumentList "/q"
 
+$Setupcmd = "setup.exe"
+$Setuppath = "$SourcePath\$SCOMver\$Setupcmd"
+.$Builddir\test-setup -setup $Setupcmd -setuppath $Setuppath
+Write-Warning "Starting $SCSCOMVER setup, this may take a while"
+
+Start-Process "$Setuppath" -ArgumentList "/install /components:$Components /ManagementGroupName:$MGMTGrp /SqlServerInstance:$INSTANCENAME /DatabaseName:OperationsManager /DWSqlServerInstance:$INSTANCENAME /DWDatabaseName:OperationsManagerDW /ActionAccountUser:$Action_ACT /ActionAccountPassword:$Password /DASAccountUser:$DAS_ACT /DASAccountPassword:$Password /DatareaderUser:$Data_Reader /DatareaderPassword:$Password /DataWriterUser:$Data_Writer /DataWriterPassword:$Password /EnableErrorReporting:Never /SendCEIPReports:0 /UseMicrosoftUpdate:0 /AcceptEndUserLicenseAgreement:1 /silent" -Wait
+   
+write-verbose "Checking for Updates"
+foreach ($Updatepattern in ("*AMD64-server.msp","*AMD64-ENU-Console.msp"))
+    {
+    $SCOMUpdate = Get-ChildItem "$($SourcePath)\$($SCOMver)updates"  -Filter $Updatepattern
+    if ($SCOMUpdate)
+        {
+        $SOMUpdate = $SCOMUpdate | Sort-Object -Property Name -Descending
+	    $LatestSCOMUpdate = $SCOMUpdate[0]
+        .$Builddir\test-setup -setup $LatestSCOMUpdate.BaseName -setuppath $LatestSCOMUpdate.FullName
+        Write-Warning "Starting SCOM Patch setup, this may take a while"
+        start-process $LatestSCOMUpdate.FullName -ArgumentList "/Passive" -Wait 
+        }
+    }
 
 if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
     {
