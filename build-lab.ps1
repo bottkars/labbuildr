9ccae6409a3c768872026a3380c98ad077988ce1
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-04 16:12:09 +0200

    changed docker to install automatcally

--- build-lab.ps1
+++ build-lab.ps1
@@ -3937,7 +3937,7 @@ switch ($PsCmdlet.ParameterSetName)
 		            $script_invoke = invoke-vmxpowershell -config $CloneVMX -Guestuser $Adminuser -Guestpassword $Adminpassword -ScriptPath $IN_Guest_UNC_NodeScriptDir -Script install-nwclient.ps1 -interactive -Parameter "-nw_ver $nw_ver"
                     }
 				invoke-postsection -wait		        
-				$script_invoke = invoke-vmxpowershell -config $CloneVMX -Guestuser $Adminuser -Guestpassword $Adminpassword -ScriptPath $IN_Guest_UNC_ScenarioScriptDir -Script install-docker.ps1 -interactive -Parameter
+				$script_invoke = invoke-vmxpowershell -config $CloneVMX -Guestuser $Adminuser -Guestpassword $Adminpassword -ScriptPath $IN_Guest_UNC_ScenarioScriptDir -Script install-docker.ps1 -interactive
 
 			}# end Cloneok
 			
