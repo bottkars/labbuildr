a44352a4ec146f251f5aafd6859a6c0eca9c4ef2
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-08 20:27:12 +0200

    added docker compose

--- install-ubuntu.ps1
+++ install-ubuntu.ps1
@@ -433,7 +433,7 @@ foreach ($Node in $machinesBuilt)
 			Write-Verbose $Scriptblock
             $Bashresult = $NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $Rootuser -Guestpassword $Guestpassword -logfile $Logfile
 
-			$Scriptblock = "curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/local/bin/docker-compose;chmod +x /usr/local/bin/docker-compose"
+			$Scriptblock = "curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-``uname -s``-``uname -m`` > /usr/local/bin/docker-compose;chmod +x /usr/local/bin/docker-compose"
 		    Write-Verbose $Scriptblock
             $Bashresult = $NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $Rootuser -Guestpassword $Guestpassword -logfile $Logfile
 			
