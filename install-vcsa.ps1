8fb94d6ed5e6648a265d091fc5f6465cf4d90a5a
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-18 13:14:31 +0200

    added install-vcsa

--- install-vcsa.ps1
+++ install-vcsa.ps1
@@ -1,12 +1,14 @@
 ﻿<#
 .Synopsis
-   .\install-Unity.ps1 -Masterpath .\Unity-1.4.5.2-535679 -Defaults
-  install-Unity only applies to Testers of the Virtual Unity
-  install-Unity is a 1 Step Process.
-  Once Unity is downloaded via feedbckcentral, run 
-   .\install-Unity.ps1 -defaults
-   This creates a Unity Master in your labbuildr directory.
-   This installs a Unity using the defaults file and the just extracted Unity Master
+   .\install-vcsa.ps1 -ovf C:\Sourcesvmware-vcsa.ova
+   .\install-vcsa.ps1 -Masterpath c:\SharedMaster -Mastername vmware-vcsa
+.Description
+  install-VCSA only applies to Testers of the Virtual VCSA
+  install-VCSA is a 2 Step Process.
+  Once VCSA is downloaded via vmware, run 
+   .\install-VCSA.ps1 -defaults
+   This creates a VCSA Master in your labbuildr directory.
+   This installs a VCSA using the defaults file and the just extracted VCSA Master
     
       
       Copyright 2016 Karsten Bott
@@ -23,31 +25,14 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 .LINK
-   https://github.com/bottkars/labbuildr/wiki/SolutionPacks#install-Unity
+   https://github.com/bottkars/labbuildr/wiki/install-VCSA.ps1
 .EXAMPLE
     Importing the ovf template
- .\install-Unity.ps1 -ovf E:\EMC_VAs\Unity-1.4.5.2-535679\Unity-1.4.5.2-535679.ovf
-    Opening OVF source: E:\EMC_VAs\Unity-1.4.5.2-535679\Unity-1.4.5.2-535679.ovf
-    The manifest does not validate
-    Opening VMX target: F:\labbuildr_beta
-    Warning:
-    - Hardware compatibility check is disabled.
-    - Line 54: Unsupported virtual hardware device 'VirtualSCSI'.
-    Writing VMX file: F:\labbuildr_beta\Unity-1.4.5.2-535679\Unity-1.4.5.2-535679.vmx
-    Transfer Completed
-    Warning:
-    - ExtraConfig option 'tools.guestlib.enableHostInfo' is not allowed, will skip it.
-    - ExtraConfig option 'sched.mem.pin' is not allowed, will skip it.
-    Completed successfully
-.EXAMPLE
-    Install a UnityNode with defaults from defaults.xml
-   .\install-Unity.ps1 -Masterpath .\Unity-1.4.5.2-535679 -Defaults
-    WARNING: VM Path does currently not exist
-    WARNING: Get-VMX : VM does currently not exist
+	.\install-vcsa.ps1 -ovf C:\Sourcesvmware-vcsa.ova
+ .EXAMPLE
+    Install a VCSANode with defaults from defaults.xml
+   .\install-vcsa.ps1 -Masterpath c:\SharedMaster -Mastername vmware-vcsa
 
-    VMXname   Status  Starttime
-    -------   ------  ---------
-    UnityNode1 Started 07.21.2015 12:27:11
 #>
 [CmdletBinding(DefaultParametersetName = "default")]
 Param(
@@ -74,6 +59,8 @@ Param(
 #requires -version 3.0
 #requires -module vmxtoolkit
 $Builddir = $PSScriptRoot
+$Password = "Password123!"
+$SSO_Domain = "vmware.local"
 switch ($PsCmdlet.ParameterSetName)
 {
 
@@ -87,7 +74,7 @@ switch ($PsCmdlet.ParameterSetName)
                 }
             catch
                 {
-                Write-Host -ForegroundColor Gray " ==> No Masterpath specified, trying default"
+                Write-Host -ForegroundColor Gray " ==>No Masterpath specified, trying default"
                 $Masterpath = $Builddir
                 }
             }
@@ -98,39 +85,67 @@ switch ($PsCmdlet.ParameterSetName)
             }
         Import-VMXOVATemplate -OVA $ovf -acceptAllEulas -AllowExtraConfig -destination $MasterPath
         #   & $global:vmwarepath\OVFTool\ovftool.exe --lax --skipManifestCheck --acceptAllEulas   --name=$mastername $ovf $PSScriptRoot #
-        Write-Host -ForegroundColor Magenta  "Use .\install-vcsa.ps1 -Masterpath $Masterpath -Mastername $Mastername 
-        .\install-Unity.ps1 -Defaults to try defaults"
+        Write-Host -ForegroundColor Magenta  "Use .\install-VCSA.ps1 -Defaults -Mastername vmware-vcsa to try defaults"
         }
 
-    default
+default
     {
-    If ($Defaults.IsPresent)
-        {
-        $labdefaults = Get-labDefaults
-        if (!($labdefaults))
-            {
-            try
-                {
-                $labdefaults = Get-labDefaults -Defaultsfile ".\defaults.xml.example"
-                }
-            catch
-                {
-                Write-Warning "no  defaults or example defaults found, exiting now"
-                exit
-                }
-            Write-Host -ForegroundColor Magenta "Using generic defaults from labbuildr"
-            }
-        $vmnet = $labdefaults.vmnet
-        $subnet = $labdefaults.MySubnet
-        $BuildDomain = $labdefaults.BuildDomain
-        $Sourcedir = $labdefaults.Sourcedir
-        $Gateway = $labdefaults.Gateway
-        $DefaultGateway = $labdefaults.Defaultgateway
-        $DNS1 = $labdefaults.DNS1
-        $DNS2 = $labdefaults.DNS2
-        $configure = $true
-        $masterpath = $labdefaults.Masterpath
-    }
+    If ($ConfirmPreference -match "none")
+		{$Confirm = $false}
+	else
+		{$Confirm = $true}
+	$Builddir = $PSScriptRoot
+	$Scriptdir = Join-Path $Builddir "Scripts"
+	If ($Defaults.IsPresent)
+		{
+		$labdefaults = Get-labDefaults
+		$vmnet = $labdefaults.vmnet
+		$subnet = $labdefaults.MySubnet
+		$BuildDomain = $labdefaults.BuildDomain
+		try
+			{
+			$Sourcedir = $labdefaults.Sourcedir
+			}
+		catch [System.Management.Automation.ValidationMetadataException]
+			{
+			Write-Warning "Could not test Sourcedir Found from Defaults, USB stick connected ?"
+			Break
+			}
+		catch [System.Management.Automation.ParameterBindingException]
+			{
+			Write-Warning "No valid Sourcedir Found from Defaults, USB stick connected ?"
+			Break
+			}
+		try
+			{
+			$Masterpath = $LabDefaults.Masterpath
+			}
+		catch
+			{
+			# Write-Host -ForegroundColor Gray " ==>No Masterpath specified, trying default"
+			$Masterpath = $Builddir
+			}
+		 $Hostkey = $labdefaults.HostKey
+		 $Gateway = $labdefaults.Gateway
+		 $DefaultGateway = $labdefaults.Defaultgateway
+		 $DNS1 = $labdefaults.DNS1
+		 $DNS2 = $labdefaults.DNS2
+		}
+	if (!$DNS2)
+		{
+		$DNS2 = $DNS1
+		}
+
+	if ($LabDefaults.custom_domainsuffix)
+		{
+		$custom_domainsuffix = $LabDefaults.custom_domainsuffix
+		}
+	else
+		{
+		$custom_domainsuffix = "local"
+		}
+
+	if (!$Masterpath) {$Masterpath = $Builddir}
 
     $Startnode = 1
     $Nodes = 1
@@ -139,10 +154,10 @@ switch ($PsCmdlet.ParameterSetName)
     $Subnet = $Subnet.major.ToString() + "." + $Subnet.Minor + "." + $Subnet.Build
 
     $Builddir = $PSScriptRoot
-    $Nodeprefix = "UnityNode"
+    $Nodeprefix = "VCSANode"
     if (!$MasterVMX)
         {
-        $MasterVMX = get-vmx -path $Masterpath -VMXName UnityVSA-4*
+        $MasterVMX = get-vmx -path $Masterpath -VMXName vmware-vcsa
         iF ($MasterVMX)
             {
             $MasterVMX = $MasterVMX | Sort-Object -Descending
@@ -159,7 +174,7 @@ switch ($PsCmdlet.ParameterSetName)
 
     if (!$MasterVMX)
         {
-        write-Host -ForegroundColor Magenta "Could not find existing UnityMaster"
+        write-Host -ForegroundColor Magenta "Could not find existing VCSAMaster"
         return
         }
     Write-Host -ForegroundColor Magenta " ==>Checking Base VM Snapshot"
@@ -172,54 +187,45 @@ switch ($PsCmdlet.ParameterSetName)
 
     if (!$Basesnap) 
         {
-        Write-Host -ForegroundColor Magenta "Tweaking Base VMX"
-        $config = Get-VMXConfig -config $MasterVMX.config
-        foreach ($scsi in 0..3)
-            {
-            $config = $config -notmatch "scsi$scsi.virtualDev"
-            $config += 'scsi'+$scsi+'.virtualDev = "pvscsi"'
-            $config = $config -notmatch "scsi$scsi.present"
-            $config += 'scsi'+$scsi+'.present = "true"'
-            }
-        Set-Content -Path $MasterVMX.config -Value $config
         Write-Host -ForegroundColor Magenta " ==>Base snap does not exist, creating now"
         $Basesnap = $MasterVMX | New-VMXSnapshot -SnapshotName Base
         }
-            # $Basesnap
+
     foreach ($Node in $Startnode..(($Startnode-1)+$Nodes))
         {
-        $ipoffset = 84+$Node
+        $ipoffset = 79+$Node
         Write-Host -ForegroundColor Magenta " ==>Checking VM $Nodeprefix$node already Exists"
         If (!(get-vmx -path $Nodeprefix$node -WarningAction SilentlyContinue))
             {
             write-Host -ForegroundColor Magenta " ==>Creating clone $Nodeprefix$node"
             $NodeClone = $MasterVMX | Get-VMXSnapshot | where Snapshot -Match "Base" | New-VMXlinkedClone -CloneName $Nodeprefix$node -Clonepath "$Builddir" 
             Write-Host -ForegroundColor Magenta " ==>Configuring NICs"
-            foreach ($nic in 0..5)
+            foreach ($nic in 0..0)
                 {
                 Write-Host -ForegroundColor Gray "  ==>Configuring NIC$nic"
                 $Netadater0 = $NodeClone | Set-VMXVnet -Adapter $nic -vnet $VMnet -WarningAction SilentlyContinue
                 }
-            Write-Host -ForegroundColor Magenta " ==>Creating Disks"
-            $SCSI = 1
-            [uint64]$Disksize = 100GB
-            if ($Disks -ne 0)
-                {
-                foreach ($LUN in (3..($Disks+2)))
-                    {
-                    $Diskname =  "SCSI$SCSI"+"_LUN$LUN.vmdk"
-                    Write-Host -ForegroundColor Gray "  ==>Building new Disk $Diskname"
-                    $Newdisk = New-VMXScsiDisk -NewDiskSize $Disksize -NewDiskname $Diskname -Verbose -VMXName $NodeClone.VMXname -Path $NodeClone.Path 
-                    Write-Host -ForegroundColor Gray "  ==>Adding Disk $Diskname to $($NodeClone.VMXname)"
-                    $AddDisk = $NodeClone | Add-VMXScsiDisk -Diskname $Newdisk.Diskname -LUN $LUN -Controller $SCSI
-                    }
-                }
+			[string]$ip="$($subnet.ToString()).$($ipoffset.ToString())"
+			$config = Get-VMXConfig -config $NodeClone.config
+			$config += "guestinfo.cis.deployment.node.type = `"embedded`""
+			$config += "guestinfo.cis.vmdir.domain-name = `"$BuildDomain.$SSO_Domain`""
+			$config += "guestinfo.cis.vmdir.site-name = `"$BuildDomain`""
+			$config += "guestinfo.cis.vmdir.password = `"$Password`""
+			$config += "guestinfo.cis.appliance.net.addr.family = `"ipv4`""
+			$config += "guestinfo.cis.appliance.net.addr = `"$ip`""
+			$config += "guestinfo.cis.appliance.net.pnid = `"$ip`""
+			$config += "guestinfo.cis.appliance.net.prefix = `"24`""
+			$config += "guestinfo.cis.appliance.net.mode = `"static`""
+			$config += "guestinfo.cis.appliance.net.dns.servers = `"$DNS1`""
+			$config += "guestinfo.cis.appliance.net.gateway = `"$DefaultGateway`""
+			$config += "guestinfo.cis.appliance.root.passwd = `"$Password`""
+			$config += "guestinfo.cis.appliance.ssh.enabled = `"true`""
+			$config | Set-Content -Path $NodeClone.config
             $Displayname = $NodeClone | Set-VMXDisplayName -DisplayName $NodeClone.CloneName
             $MainMem = $NodeClone | Set-VMXMainMemory -usefile:$false
-            $Annotation = $Nodeclone | Set-VMXAnnotation -Line1 "System User" -Line2 "sysadmin:sysadmin" -Line3 "Unity User" -Line4 "admin:Password123#"
+            $Annotation = $Nodeclone | Set-VMXAnnotation -Line1 "Login Credentials" -Line2 "Administrator@$BuildDomain.$SSO_Domain" -Line3 "Password" -Line4 "$Password"
             Write-Host -ForegroundColor Magenta " ==>Starting VM $($NodeClone.Clonename)"
             $NodeClone | start-vmx | Out-Null
-            [string]$ip="$($subnet.ToString()).$($ipoffset.ToString())"
             }
         else
             {
@@ -227,18 +233,10 @@ switch ($PsCmdlet.ParameterSetName)
             }
     }
 Write-host
-Write-host -ForegroundColor White "****** To Configure  Unity 4 ******
-        Go to VMware Console an wait for system to boot
-        It might take up to 15 Minutes on First boot
-        Login with  
-service/service 
-        and run  
-svc_initial_config -4 `"$ip 255.255.255.0 $DefaultGateway`"
-        once configured, open browser to 
-https://$ip and login with admin / Password123#
-    activate your license at
- https://www.emc.com/auth/elmeval.htm
- Please keep your license in a save location as it might me re-used when re-deploying $Nodeprefix$Node
+Write-host -ForegroundColor White "****** VCSA Deployed successful******
+give 10 minutes to warm Up, then browse to
+https://$ip and login with Administrator@$BuildDomain.$SSO_Domain / Password123#
+
 "
 
     }# end default
