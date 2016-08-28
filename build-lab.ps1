8ba9557784d320f297f8a6f954d1eccef1cfa0f9
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-28 11:38:08 +0200

    attache

--- build-lab.ps1
+++ build-lab.ps1
@@ -2071,13 +2071,14 @@ if ($Exchange2013.IsPresent)
      foreach ($URL in $attachments)
         {
         $FileName = Split-Path -Leaf -Path $Url
-        if (!(test-path  "$Destination\$FileName"))
+		$Destination_file = Join-Path $Destination $FileName
+        if (!(test-path  $Destination_file))
             {
             Write-Verbose "$FileName not found, trying Download"
-            if (!(Receive-LABBitsFile -DownLoadUrl $URL -destination $Sourcedir\$Prereqdir\$FileName))
+            if (!(Receive-LABBitsFile -DownLoadUrl $URL -destination $Destination_file))
                 { Write-Host -ForegroundColor Gray " ==>Error Downloading file $Url, Please check connectivity"
                   Write-Host -ForegroundColor Gray " ==>creating dummy File"
-                  New-Item -ItemType file -Path "$Sourcedir\$Prereqdir\$FileName" | out-null
+                  New-Item -ItemType file -Path $Destination_file | out-null
                 }
             }
         }
@@ -2113,13 +2114,14 @@ if ($Exchange2016.IsPresent)
      foreach ($URL in $attachments)
         {
         $FileName = Split-Path -Leaf -Path $Url
-        if (!(test-path  "$Destination\$FileName"))
+		$Destination_file = Join-Path $Destination $FileName
+        if (!(test-path  $Destination_file))
             {
             Write-Verbose "$FileName not found, trying Download"
-            if (!(Receive-LABBitsFile -DownLoadUrl $URL -destination $Sourcedir\$Prereqdir\$FileName))
+            if (!(Receive-LABBitsFile -DownLoadUrl $URL -destination $Destination_file))
                 { Write-Host -ForegroundColor Gray " ==>Error Downloading file $Url, Please check connectivity"
                   Write-Host -ForegroundColor Gray " ==>creating dummy File"
-                  New-Item -ItemType file -Path "$Sourcedir\$Prereqdir\$FileName" | out-null
+                  New-Item -ItemType file -Path $Destination_file | out-null
                 }
             }
         }
