1c22649ba29c78a3eb5c6edc395e2b5297f2b408
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-29 12:36:55 +0200

    git updater

--- update.ps1
+++ update.ps1
@@ -1,6 +1,16 @@
-git -C $HOME/labbuildr pull
-git -C $HOME/labbuildr/labbuildr-scripts pull
-git -C $HOME/labbuildr/vmxtoolkit pull
-git -C $HOME/labbuildr/labtools pull
-ipmo $HOME/labbuildr/vmxtoolkit -ArgumentList "$HOME/labbuildr/" -Force
-ipmo $HOME/labbuildr/labtools -Force 
+if (git)
+	{
+	Write-Host -ForegroundColor Gray " ==> git installed, running update"
+	git -C $HOME/labbuildr pull
+	git -C $HOME/labbuildr/labbuildr-scripts pull
+	git -C $HOME/labbuildr/vmxtoolkit pull
+	git -C $HOME/labbuildr/labtools pull
+	ipmo $HOME/labbuildr/vmxtoolkit -ArgumentList "$HOME/labbuildr/" -Force
+	ipmo $HOME/labbuildr/labtools -Force 
+	}
+else
+	{
+	Write-Host -ForegroundColor Yellow " ==>Sorry, you need git to run Update"
+	}
+
+
