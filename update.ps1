09d3039a3dbc1b5e149796a2b479300a7abaa5af
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-29 12:41:22 +0200

    added update

--- update.ps1
+++ update.ps1
@@ -1,10 +1,12 @@
 if (git)
 	{
 	Write-Host -ForegroundColor Gray " ==> git installed, running update"
-	git -C $HOME/labbuildr pull
-	git -C $HOME/labbuildr/labbuildr-scripts pull
-	git -C $HOME/labbuildr/vmxtoolkit pull
-	git -C $HOME/labbuildr/labtools pull
+	$Repos = ('/labbuildr','/labbuildr/labbuildr-scripts','/labbuildr/vmxtoolkit','/labbuildr/labtools')
+	foreach ($Repo in $Repos)
+		{
+		Write-Host -ForegroundColor Gray " ==>Checking for Update on $Repo"
+		git -C $HOME/$Repo pull
+		}
 	ipmo $HOME/labbuildr/vmxtoolkit -ArgumentList "$HOME/labbuildr/" -Force
 	ipmo $HOME/labbuildr/labtools -Force 
 	}
