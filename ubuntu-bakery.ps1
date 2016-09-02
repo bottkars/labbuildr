021b454990797565894d022d243ed76da65ac7fa
Karsten Bott <Karsten.Bott@bottnet.de>
2016-09-02 13:05:12 +0200

    gwpassword, percentage

--- ubuntu-bakery.ps1
+++ ubuntu-bakery.ps1
@@ -234,8 +234,11 @@ if ($scaleio.IsPresent)
 	$Ubuntu_guestdir = $Ubuntu_guestdir.Replace("\","/")
     Write-Host $Ubuntu_guestdir
     Write-Host $SIOGatewayrpm
-	
-		
+	$Percentage = [math]::Round(100/$nodes)+1
+	if ($Percentage -lt 10)
+		{
+		$Percentage = 10
+		}	
 	}
 
 switch ($ubuntu_ver)
@@ -547,10 +550,9 @@ if ($scaleio.IsPresent)
 					$Scriptblock = "add-apt-repository ppa:webupd8team/java -y;apt-get update -y;echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections;apt-get install oracle-java8-installer -y;apt-get install oracle-java8-set-default -y"
 					Write-Host $Scriptblock
 					$NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $rootuser -Guestpassword $Guestpassword -logfile $Logfile | Out-Null
-					$Scriptblock = "GATEWAY_ADMIN_PASSWORD=Password123! /usr/bin/dpkg -i $SIOGatewayrpm"
+					$Scriptblock = "/usr/bin/dpkg -i $SIOGatewayrpm"
 					#$NodeClone | Invoke-VMXBash -Scriptblock "export SIO_GW_KEYTOOL=/usr/bin/;export GATEWAY_ADMIN_PASSWORD='Password123!';dpkg -i $SIOGatewayrpm;dpkg -l emc-scaleio-gateway" -Guestuser $rootuser -Guestpassword $Guestpassword -logfile $Logfile | Out-Null
 					Write-Host $Scriptblock
-					pause
 					$NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $rootuser -Guestpassword $Guestpassword  | Out-Null #-logfile $Logfile
 					if (!$singlemdm)
 						{
