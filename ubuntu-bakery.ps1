c44f3bf8f26bfb21d580bff07a9558c90e97d7dc
Karsten Bott <Karsten.Bott@bottnet.de>
2016-09-15 08:49:46 +0200

    bash substitute

--- ubuntu-bakery.ps1
+++ ubuntu-bakery.ps1
@@ -708,7 +708,7 @@ curl --silent --show-error --insecure --user :`$TOKEN -X POST -H 'Content-Type: 
 	if ($Openstack_Controller.IsPresent)
 		{
 		Write-Host -ForegroundColor Gray " ==>starting OpenStack controller setup on $($GatewayNode.VMXName)"
-		$Scriptblock = "cd /mnt/hgfs/Scripts/openstack/Controller; sh ./install_base.sh -spd $ProtectionDomainName -ssp $StoragePoolName -sgw $tb_ip -d $BuildDomain -c $($Openstack_Baseconfig.ispresent.ToString().tolower())"
+		$Scriptblock = "cd /mnt/hgfs/Scripts/openstack/Controller; bash ./install_base.sh -spd $ProtectionDomainName -ssp $StoragePoolName -sgw $tb_ip -d $BuildDomain -c "#$($Openstack_Baseconfig.ispresent.ToString().tolower())"
 		$GatewayNode | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $rootuser -Guestpassword $Guestpassword -logfile $logfile | Out-Null
 		$installmessage += "OpenStack Horizon can be reached via http://$($tb_ip):88/horizon with admin:$($Guestpassword)`n"
 		foreach ($Node in $machinesBuilt)
@@ -717,7 +717,7 @@ curl --silent --show-error --insecure --user :`$TOKEN -X POST -H 'Content-Type: 
 			if ($NodeClone.vmxname -ne $GatewayNode.vmxname)
 				{
 				Write-Host -ForegroundColor Gray " ==>starting nova-compute setup on $($NodeClone.vmxname)"
-				$Scriptblock = "cd /mnt/hgfs/Scripts/openstack/Compute; sh ./install_base.sh -cip $tb_ip -cname $($GatewayNode.vmxname.tolower())"
+				$Scriptblock = "cd /mnt/hgfs/Scripts/openstack/Compute; bash ./install_base.sh -cip $tb_ip -cname $($GatewayNode.vmxname.tolower())"
 				Write-Verbose $Scriptblock
 				$NodeClone| Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $rootuser -Guestpassword $Guestpassword -logfile $Logfile | Out-Null
 				$installmessage += "OpenStack Nova-Compute is running on $($NodeClone.vmxname)`n"
