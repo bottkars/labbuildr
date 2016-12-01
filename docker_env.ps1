91206e4931dcf58ca556dfe5edfe80a48bbfcc36
Karsten Bott <Karsten.Bott@bottnet.de>
2016-12-01 17:10:57 +0100

    docker

--- docker_env.ps1
+++ docker_env.ps1
@@ -22,8 +22,14 @@ write-host -ForegroundColor Yellow '
               \____\_______/
 
 '
-Write-Host "Welcome to labbuildr Boot2Docker Environment"
+Write-Host "Welcome to labbuildr Boot2Docker Environment
+you are running on VMware $vmxtoolkit_type $($$vmwareversion.ToString()) "
 Write-Host -ForegroundColor White "Active Docker-Machine Hosts:"
 docker-machine ls
-write-host "To remove all docker machines, run: 
+write-host "
+create a docker test machine:
+docker-machine create test --driver vmwareworkstation
+docker-machine env test | Invoke-Expression
+
+To remove all docker machines, run: 
 docker-machine ls -q | foreach { docker-machine rm $_}"
