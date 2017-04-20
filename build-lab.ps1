be8532b70c98406ba6088aa32755b09d0fa28471
Karsten Bott <Karsten.Bott@bottnet.de>
2017-04-20 11:36:57 +0200

    new nw version

--- build-lab.ps1
+++ build-lab.ps1
@@ -508,9 +508,9 @@ Machine Sizes
     <#
 Version Of Networker Modules
     'nmm9100','nmm9102','nmm9103','nmm9104','nmm9105',#-#
-    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016',#
+    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016','nmm9017','nmm9018',#
     'nmm90.DA','nmm9001','nmm9002','nmm9003','nmm9004','nmm9005','nmm9006','nmm9007','nmm9008',
-	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244',#-#
+	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244','nmm8245',#-#
 	'nmm230','nmm8231','nmm8232','nmm8233','nmm8235','nmm8236','nmm8237','nmm8238',
     'nmm8221','nmm8222','nmm8223','nmm8224','nmm8225','nmm8226',
     'nmm8218','nmm8217','nmm8216','nmm8214','nmm8212','nmm8210'
@@ -526,9 +526,9 @@ Version Of Networker Modules
     [Parameter(ParameterSetName = "SCOM", Mandatory = $false)]
     [Parameter(ParameterSetName = "Sharepoint", Mandatory = $false)]
     [ValidateSet('nmm9100','nmm9102','nmm9103','nmm9104','nmm9105',#-#
-    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016',#
+    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016','nmm9017','nmm9018',#
     'nmm90.DA','nmm9001','nmm9002','nmm9003','nmm9004','nmm9005','nmm9006','nmm9007','nmm9008',
-	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244',#-#
+	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244','nmm8245',#-#
 	'nmm230','nmm8231','nmm8232','nmm8233','nmm8235','nmm8236','nmm8237','nmm8238',
     'nmm8221','nmm8222','nmm8223','nmm8224','nmm8225','nmm8226',
     'nmm8218','nmm8217','nmm8216','nmm8214','nmm8212','nmm8210'
@@ -555,9 +555,9 @@ Version Of Networker Modules
     <#
 Version Of Networker Server / Client to be installed
 	'nw9100','nw9102','nw9103','nw9104','nw9105',#-#
-    'nw9010','nw9011','nw9012','nw9013','nw9014','nw9015','nw9016',#
+    'nw9010','nw9011','nw9012','nw9013','nw9014','nw9015','nw9016','nw9017','nw9018',#
     'nw90.DA','nw9001','nw9002','nw9003','nw9004','nw9005','nw9006','nw9007','nw9008',
-	'nw8240','nw8241','nw8242','nw8243','nw8244','nw8245'#-#
+	'nw8240','nw8241','nw8242','nw8243','nw8244','nw8245',#-#
     'nw8230','nw8231','nw8232','nw8233','nw8234','nw8235','nw8236','nw8237','nw8238',
     'nw8226','nw8225','nw8224','nw8223','nw8222','nw8221','nw822',
     'nw8218','nw8217','nw8216','nw8215','nw8214','nw8213','nw8212','nw8211','nw8210',
@@ -593,9 +593,9 @@ Version Of Networker Server / Client to be installed
 	[Parameter(ParameterSetName = "docker", Mandatory = $false)]
     [ValidateSet(
 	'nw9100','nw9102','nw9103','nw9104','nw9105',#-#
-    'nw9010','nw9011','nw9012','nw9013','nw9014','nw9015','nw9016',#
+    'nw9010','nw9011','nw9012','nw9013','nw9014','nw9015','nw9016','nw9017','nw9018',#
     'nw90.DA','nw9001','nw9002','nw9003','nw9004','nw9005','nw9006','nw9007','nw9008',
-	'nw8240','nw8241','nw8242','nw8243','nw8244','nw8245'#-#
+	'nw8240','nw8241','nw8242','nw8243','nw8244','nw8245',#-#
     'nw8230','nw8231','nw8232','nw8233','nw8234','nw8235','nw8236','nw8237','nw8238',
     'nw8226','nw8225','nw8224','nw8223','nw8222','nw8221','nw822',
     'nw8218','nw8217','nw8216','nw8215','nw8214','nw8213','nw8212','nw8211','nw8210',
