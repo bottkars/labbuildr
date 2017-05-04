fa4f77a3953313068405cb23eb7e644bb4c2bae0
bottkars <Karsten.Bott@bottnet.de>
2017-05-04 14:38:47 +0200

    doku

--- build-lab.ps1
+++ build-lab.ps1
@@ -2397,68 +2397,6 @@ if ($Sharepoint.IsPresent)
 			}
 		}
 
-<#    $Prereqdir = "$spver"+"prereq"
-    Write-Verbose " ==>we are now going to Test Sharepoint Prereqs"
-    $DownloadUrls = (
-		    "http://download.microsoft.com/download/9/1/3/9138773A-505D-43E2-AC08-9A77E1E0490B/1033/x64/sqlncli.msi", # Microsoft SQL Server 2008 R2 SP1 Native Client
-		    "http://download.microsoft.com/download/E/0/0/E0060D8F-2354-4871-9596-DC78538799CC/Synchronization.msi", # Microsoft Sync Framework Runtime v1.0 SP1 (x64)
-		    "http://download.microsoft.com/download/A/6/7/A678AB47-496B-4907-B3D4-0A2D280A13C0/WindowsServerAppFabricSetup_x64.exe", # Windows Server App Fabric
-            "http://download.microsoft.com/download/7/B/5/7B51D8D1-20FD-4BF0-87C7-4714F5A1C313/AppFabric1.1-RTM-KB2671763-x64-ENU.exe", # Cumulative Update Package 1 for Microsoft AppFabric 1.1 for Windows Server (KB2671763)
-			# "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49171&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1", # Cumulative Update 7 (KB3092423) for Microsoft AppFabric 1.1 for Windows Server
-            "http://download.microsoft.com/download/D/7/2/D72FD747-69B6-40B7-875B-C2B40A6B2BDD/Windows6.1-KB974405-x64.msu", #Windows Identity Foundation (KB974405)
-		    "http://download.microsoft.com/download/0/1/D/01D06854-CA0C-46F1-ADBA-EBF86010DCC6/rtm/MicrosoftIdentityExtensions-64.msi", # Microsoft Identity Extensions
-		    "http://download.microsoft.com/download/9/1/D/91DA8796-BE1D-46AF-8489-663AB7811517/setup_msipc_x64.msi", # Microsoft Information Protection and Control Client
-		    "http://download.microsoft.com/download/8/F/9/8F93DBBD-896B-4760-AC81-646F61363A6D/WcfDataServices.exe" # Microsoft WCF Data Services 5.0
-                )
-    if (Test-Path $labbuildr_sourcedir/$Prereqdir)
-        {
-        Write-Verbose "Sharepoint Prereq Sourcedir Found"
-        }
-        else
-        {
-        Write-Verbose "==>creating Prereq Sourcedir for Sharepoint"
-        New-Item -ItemType Directory -Path $labbuildr_sourcedir/$Prereqdir | Out-Null
-        }
-    foreach ($URL in $DownloadUrls)
-        {
-        $FileName = Split-Path -Leaf -Path $Url
-        Write-Verbose "...checking for $FileName"
-        if (!(test-path  $labbuildr_sourcedir/$Prereqdir/$FileName))
-            {
-            Write-Verbose "$FileName not found, trying Download"
-            if (!(get-prereq -DownLoadUrl $URL -destination $labbuildr_sourcedir\$Prereqdir\$FileName))
-                {
-                Write-Warning "Error Downloading file $Url, Please check connectivity"
-                exit
-                }
-            }
-        }
-        $URL = "http://download.microsoft.com/download/1/C/A/1CAA41C7-88B9-42D6-9E11-3C655656DAB1/WcfDataServices.exe"
-        $FileName = "WcfDataServices56.exe"
-        Write-Verbose "...checking for $FileName"
-        if (!(test-path  $labbuildr_sourcedir/$Prereqdir/$FileName))
-            {
-            Write-Verbose "$FileName not found, trying Download"
-            if (!(get-prereq -DownLoadUrl $URL -destination $labbuildr_sourcedir/$Prereqdir/$FileName))
-                {
-                Write-Warning "Error Downloading file $Url, Please check connectivity"
-                exit
-                }
-            }
-        $Url = "http://download.microsoft.com/download/6/E/3/6E3A0B03-F782-4493-950B-B106A1854DE1/sharepoint.exe"
-        Write-Verbose "Testing Sharepoint SP1 Foundation exists in $labbuildr_sourcedir"
-        if (!(test-path  "$labbuildr_sourcedir/$SPver"))
-            {
-            $FileName = Split-Path -Leaf -Path $Url
-            Write-Verbose "Trying Download"
-            if (!(get-prereq -DownLoadUrl $URL -destination  "$labbuildr_sourcedir/$FileName"))
-                {
-                Write-Warning "Error Downloading file $Url, Please check connectivity"
-                exit
-                }
-            Write-Verbose "Extracting $FileName"
-            Start-Process -FilePath "$labbuildr_sourcedir/$FileName" -ArgumentList "/extract:$labbuildr_sourcedir/$SPver /quiet /passive" -Wait
-            } #>
     $Work_Items +=  " ==>we are going to Install $SP_Ver in Domain $BuildDomain with Subnet $MySubnet using VMnet$VMnet and SQL"
     }# end SPPREREQ
 if ($ConfigureVMM.IsPresent)
