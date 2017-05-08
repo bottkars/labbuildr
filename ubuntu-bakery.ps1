c5f1a74fee8d46188c1b3ab7ef75276879bdb073
bottkars <Karsten.Bott@bottnet.de>
2017-05-08 07:23:44 +0200

    debug

--- ubuntu-bakery.ps1
+++ ubuntu-bakery.ps1
@@ -398,6 +398,12 @@ switch ($ubuntu_ver)
         }
     }
 
+if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
+    {
+		Write-Verbose $ubuntu_sio_ver
+		Write-Verbose $Ubuntudir
+		Pause
+	}	
 $scsi = 0
 $Nodeprefix = "Ubuntu"
 $Nodeprefix = $Nodeprefix.ToLower()
