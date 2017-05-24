3c7b5075f9de2c76046ecb3d344a9a81bcd2d926
Karsten Bott <Karsten.Bott@bottnet.de>
2017-05-24 15:26:38 +0200

    typo

--- install-ecs3.ps1
+++ install-ecs3.ps1
@@ -388,7 +388,7 @@ Write-Verbose $Scriptblock
 $Bashresult = $NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $Rootuser -Guestpassword $Guestpassword #-logfile $Logfile
 
 Write-Host -ForegroundColor White "Starting ECS Configuration Step1, this may take a while"
-Write-Host -ForegroundColor White "you may follow the process with 'tail -f ls /tmp/systemd-private-*-vmtoolsd.service-*/tmp/labbuildr.log"
+Write-Host -ForegroundColor White "you may follow the process with 'tail -f /tmp/systemd-private-*-vmtoolsd.service-*/tmp/labbuildr.log"
 $Scriptblock = 'cd /ECS-CommunityEdition; /root/bin/step1'
 Write-Verbose $Scriptblock
 $Bashresult = $NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $Rootuser -Guestpassword $Guestpassword -logfile $Logfile
