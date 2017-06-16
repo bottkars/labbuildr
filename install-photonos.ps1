040a4735ff745dd0f74d19ba2d7e0629f19b3164
Karsten Bott labbuildr <karsten.bott@labbuildr.com>
2017-06-16 20:17:47 +0200

    added photon stage 2

--- install-photonos.ps1
+++ install-photonos.ps1
@@ -29,12 +29,15 @@ this will install a Puppetmaster on CentOS7 using default Values derived from de
 Param(
     [Parameter(ParameterSetName = "install", Mandatory = $false)]
     [ValidateScript( { Test-Path -Path $_ -ErrorAction SilentlyContinue })]$Sourcedir = $labdefaults.Sourcedir,
-    [Parameter(ParameterSetName = "install",Mandatory=$false)][ValidateScript({$_ -match [IPAddress]$_ })][ipaddress]$subnet = $labdefaults.MySubnet,
+    [Parameter(ParameterSetName = "install", Mandatory = $false)][ValidateScript( {$_ -match [IPAddress]$_ })][ipaddress]$subnet = $labdefaults.MySubnet,
     [Parameter(ParameterSetName = "install", Mandatory = $False)]
     [ValidateLength(1, 15)][ValidatePattern("^[a-zA-Z0-9][a-zA-Z0-9-]{1,15}[a-zA-Z0-9]+$")][string]$BuildDomain = $labdefaults.BuildDomain,
     [Parameter(ParameterSetName = "install", Mandatory = $false)][ValidateSet('vmnet1', 'vmnet2', 'vmnet3')]$vmnet = $labdefaults.vmnet,
     [Parameter(ParameterSetName = "install", Mandatory = $false)][ValidateSet('photon-1.0-rev2')]$photonOS = 'photon-1.0-rev2',
     [Parameter(ParameterSetName = "install", Mandatory = $false)]$masterpath = $labdefaults.Masterpath,
+    [Parameter(ParameterSetName = "install", Mandatory = $false)]$DNS1 = $labdefaults.DNS1,
+    [Parameter(ParameterSetName = "install", Mandatory = $false)]$DefaultGateway = $labdefaults.DefaultGateway,
+    [Parameter(ParameterSetName = "install", Mandatory = $false)]$Hostkey = $labdefaults.Hostkey,
     $Startnode = 1,
     $nodes = 1
 )
@@ -47,75 +50,54 @@ $Subnet = $Subnet.major.ToString() + "." + $Subnet.Minor + "." + $Subnet.Build
 $Master_StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
 $masterVMX = Test-LABmaster -Masterpath $masterpath -Master $photonOS
 $Master_StopWatch.stop()
-
+New-Item -ItemType Directory ./labbuildr-scripts/Photon/config-drive -Force | Out-Null
 foreach ($Node in $Startnode..(($Startnode - 1) + $Nodes)) {
     if (!(get-vmx $Nodeprefix$node -WarningAction SilentlyContinue)) {   
         write-verbose "Creating $Nodeprefix$node"
         $NodeClone = $MasterVMX | Get-VMXSnapshot | where Snapshot -Match "Base" | New-VMXLinkedClone -CloneName $Nodeprefix$Node 
+        $IP = "$subnet.4$Node"
+        $meta_Data = "instance-id: iid-local01
+local-hostname: cloudimg"
 
-
-        <#  $User_data= "#cloud-config
+        $User_data = "#cloud-config
 hostname: $($nodeclone.clonename)
 ssh_authorized_keys:
     - $($Hostkey)
+chpasswd:
+  list: |  
+    root:Password123!
+    expire: False    
 write_files:
-    - path: /etc/systemd/network/static.network
+    - path: /etc/systemd/network/10-static-en.network
       permissions: 0644
-      content: |
-        [Match]
-        Name=eno16777984
-
-        [Network]
-        Address=$IP/24
-        Gateway=$DefaultGateway
-        DNS=$DNS1
-        DNS=8.8.8.8
-    - path: /etc/iptables.rules
+      content: | 
+       [Match]
+       Name=eth0
+       [Network]
+       Address=$IP/24
+       Gateway=$DefaultGateway
+       DNS=$DNS1
+       DNS=8.8.8.8
+    - path: /etc/systemd/network/10-dhcp-en.network
       permissions: 0644
-      content: |
-        *filter
-        :INPUT DROP [0:0]
-        :FORWARD ACCEPT [0:0]
-        :OUTPUT ACCEPT [76:7696]
-        -A INPUT -p tcp -m conntrack --ctstate NEW -m multiport --dports 22 -j ACCEPT
-        -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-        -A INPUT -i lo -j ACCEPT
-        -A INPUT -p icmp --icmp-type 8 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-        -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
-        COMMIT
-coreos:
-    units:
-        - name: systemd-networkd.service
-          command: restart
-        - name: iptables.service
-          command: start
-          content: |
-            [Unit]
-            Description=iptables
-            Author=Me
-            After=systemd-networkd.service
+      content: | 
 
-            [Service]
-            Type=oneshot
-            ExecStart=/usr/sbin/iptables-restore /etc/iptables.rules
-            ExecReload=/usr/sbin/iptables-restore /etc/iptables.rules
-            ExecStop=/usr/sbin/iptables-restore /etc/iptables.rules
-
-            [Install]
-            WantedBy=multi-user.target"
-
-		$User_data | Set-Content -Path "$PSScriptRoot/labbuildr-scripts/CoreOS/config-drive/openstack/latest/user_data" | Out-Null 
-		convert-VMXdos2unix -Sourcefile "$PSScriptRoot/labbuildr-scripts/CoreOS/config-drive/openstack/latest/user_data" | Out-Null 
-		Write-Host -ForegroundColor Gray "  ==>Creating config-2 CD"
-		.$global:mkisofs -r -V config-2 -o "$($NodeClone.path)/config.iso"  "$PSScriptRoot/Scripts/CoreOS/config-drive" #  | Out-Null
-
-		$NodeClone | Connect-VMXcdromImage -ISOfile "$($NodeClone.path)/config.iso" -Contoller ide -Port 1:0 | Out-Null 
-		$NodeClone | Set-VMXNetworkAdapter -Adapter 0 -ConnectionType custom -AdapterType vmxnet3 -WarningAction SilentlyContinue | Out-Null 
-		$NodeClone | Set-VMXVnet -Adapter 0 -Vnet $vmnet -WarningAction SilentlyContinue | Out-Null 
-		$NodeClone | Set-VMXDisplayName -DisplayName "$($NodeClone.Clonename)@$BuildDomain" | Out-Null 
-		$Content = $Nodeclone | Get-VMXConfig 
-		$Content = $Content -replace 'preset','soft'
-		$Content | Set-Content -Path $NodeClone.config #>
+runcmd:
+    - systemctl restart systemd-networkd
+"
+        $User_data | Set-Content -Path "$PSScriptRoot/labbuildr-scripts/Photon/config-drive/user-data" | Out-Null 
+        $meta_Data | Set-Content -Path "$PSScriptRoot/labbuildr-scripts/Photon/config-drive/meta-data" | Out-Null 
+        convert-VMXdos2unix -Sourcefile "$PSScriptRoot/labbuildr-scripts/Photon/config-drive/user-data" | Out-Null 
+        convert-VMXdos2unix -Sourcefile "$PSScriptRoot/labbuildr-scripts/Photon/config-drive/meta-data" | Out-Null
+        Write-Host -ForegroundColor Gray "  ==>Creating seed iso"
+        .$global:mkisofs -R -J -V cidata -o "$($NodeClone.path)/seed.iso"  "$PSScriptRoot/labbuildr-scripts/Photon/config-drive" #  | Out-Null
+        $NodeClone | Connect-VMXcdromImage -ISOfile "$($NodeClone.path)/seed.iso" -Contoller ide -Port 1:0 | Out-Null 
+        $NodeClone | Set-VMXNetworkAdapter -Adapter 0 -ConnectionType custom -AdapterType vmxnet3 -WarningAction SilentlyContinue | Out-Null 
+        $NodeClone | Set-VMXVnet -Adapter 0 -Vnet $vmnet -WarningAction SilentlyContinue | Out-Null 
+        $NodeClone | Set-VMXDisplayName -DisplayName "$($NodeClone.Clonename)@$BuildDomain" | Out-Null 
+        $Content = $Nodeclone | Get-VMXConfig 
+        $Content = $Content -replace 'preset', 'soft'
+        $Content | Set-Content -Path $NodeClone.config #>
         $NodeClone | start-vmx | Out-Null 
     }#end machine
 
