3cc05d415b7f13977022ca97905307ff6a7c03da
Karsten Bott <Karsten.Bott@bottnet.de>
2017-07-03 20:45:48 +0200

    silent

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,3 @@
 Write-Warning "you reached emergency update mode, patching to new labbuildr Distro Version"
-$Filecopy = Copy-Item ./labbuildr/build-lab.ps1 -Force -Destination ./build-lab.ps1
+$Filecopy = Copy-Item ./labbuildr/build-lab.ps1 -Force -Destination ./build-lab.ps1 -ErrorAction SilentlyContinue
 ./build-lab.ps1 -update -Force
