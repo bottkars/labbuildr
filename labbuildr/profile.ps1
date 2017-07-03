ef8dffb370024d01a781ffbc0da63468429dbbbb
Karsten Bott <Karsten.Bott@bottnet.de>
2017-07-03 19:45:52 +0200

    force

--- labbuildr/profile.ps1
+++ labbuildr/profile.ps1
@@ -114,4 +114,4 @@ else
 	}
 Set-LabUI
 Get-VMX
-Write-Host -ForegroundColor white  "If upgraded from Asparagus release, plese run update twice"
+Write-Host -ForegroundColor white  "If upgraded from Asparagus release, plese run update twice with -force"
