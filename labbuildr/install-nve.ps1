53f1da325f628967300dfb902a493e889f19c2a3
Karsten Bott <Karsten.Bott@bottnet.de>
2017-09-13 21:49:21 +0200

    tag

--- labbuildr/install-nve.ps1
+++ labbuildr/install-nve.ps1
@@ -130,7 +130,7 @@ switch ($PsCmdlet.ParameterSetName)
             Write-host -ForegroundColor Gray " ==> OVF does not exist, we need to extract from OVA" 
             Write-Host -ForegroundColor Gray " ==> Checking for $Product OVA package"
             Receive-LABNetworker -nve -nve_ver $nve_ver -Destination "$Sourcedir" -Confirm:$false
-            $OVA_File = Get-ChildItem -Path "$Sourcedir\$Product" -Recurse -include "$Product_tag.ova" -Exclude ".*" | Sort-Object -Descending | Select-Object -First 1
+            $OVA_File = Get-ChildItem -Path "$Sourcedir\$Product" -Recurse -include "*$Product_tag.ova "-Exclude ".*" | Sort-Object -Descending | Select-Object -First 1
             if (!$OVA_File)
 				{
 				Write-Host "Could not find ova with pattern *$Product_tag.ova in $Sourcedir\$Product"
