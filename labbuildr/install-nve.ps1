17ab226ff285692b8c201655d5912e683ee60c00
Karsten Bott <Karsten.Bott@bottnet.de>
2017-09-18 08:00:28 +0200

    new nve

--- labbuildr/install-nve.ps1
+++ labbuildr/install-nve.ps1
@@ -29,6 +29,7 @@ Param(
 [Parameter(ParameterSetName = "defaults",Mandatory = $true)]
 [Parameter(ParameterSetName = "import",Mandatory = $false)]
 [ValidateSet(
+	'9.2.0.3',
 	'9.1.1.1','9.1.1.2','9.1.1.3', #-#
     '9.0.1-72',
 	'9.1.0.3','9.1.0.4',#-#
@@ -111,7 +112,11 @@ switch ($nve_ver)
 		'9.1.1.3'
 		{
 		$Product_tag = '9.1.1.188'
-		}		
+		}
+		'9.2.0.3'
+		{
+		$Product_tag = '9.2.0.128'
+		}				
 	}
 
 switch ($PsCmdlet.ParameterSetName)
